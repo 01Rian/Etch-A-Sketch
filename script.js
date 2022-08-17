5782619a43f41f9b3b4e52cc2ebf4bee879953ae
Rian <riansantosjr11@gmail.com>
2022-08-17 13:28:59 -0300

    add create grid func

--- script.js
+++ script.js
@@ -28,3 +28,35 @@ function defaultGrid() {
 }
 
 defaultGrid();
+
+function createGrid() {
+    if (!input.value) {
+        alert('please set a grid size');
+    } else {
+        grid.innerHTML = '';
+        grid.style.setProperty('grid-template-columns', `repeat(${input.value}, 2fr)`);
+        grid.style.setProperty('grid-template-rows', `repeat(${input.value}, 2fr)`);
+        place.innerHTML = input.value + 'x' + input.value;
+
+        for (let i = 0; i < input.value * input.value; i++) {
+            const div = document.createElement('div');
+            div.classList.add('colorGrid');
+            grid.appendChild(div);
+        }
+
+        const allColors = document.querySelectorAll('.colorGrid');
+        for (color of allColors) {
+            color.addEventListener('mouseover', (e) => {
+                const colorBackground = getComputedStyle(e.target).backgroundColor;
+                const regex = /\d\.\d/;
+        
+                if (colorBackground.match(regex) !== null) {
+                    const colorOpacity = Number(colorBackground.match(regex));
+                    e.target.style.backgroundColor = colorBackground.replace(regex, colorOpacity + 0.2);
+                }
+            });
+        }
+    }
+}
+
+sizeGrid.addEventListener('click', createGrid);
