835b90451d26949adf8657444b7f18ee2e0fc145
Rian <riansantosjr11@gmail.com>
2023-05-25 00:19:23 -0300

    code refactoring for module pattern and add mouse click color change event

--- script.js
+++ script.js
@@ -1,60 +1,82 @@
-const place = document.querySelector('.placeGrid');
-const input = document.querySelector('#quantity');
-const sizeGrid = document.querySelector('.create');
-const reset = document.querySelector('.reset');
-const grid = document.querySelector('.grid');
-
-function defaultGrid() {
-    for (let i = 0; i < 16 * 16; i++) {
+const app = (() => {
+    const place = document.querySelector('.placeGrid');
+    const input = document.querySelector('#quantity');
+    const sizeGrid = document.querySelector('.create');
+    const reset = document.querySelector('.reset');
+    const grid = document.querySelector('.grid');
+    let isMouseDown = false;
+  
+    const defaultGrid = () => {
+      for (let i = 0; i < 16 * 16; i++) {
         const div = document.createElement('div');
         div.classList.add('colorGrid');
         grid.appendChild(div);
-        grid.style.setProperty("grid-template-columns", `repeat(16, 2fr)`);
-        grid.style.setProperty("grid-template-rows", `repeat(16, 2fr)`);
-    }
-    colorGrid();
-}
-
-function colorGrid() {
-    const allColors = document.querySelectorAll('.colorGrid');
-    for (color of allColors) {
-        color.addEventListener('mouseover', (e) => {
+        grid.style.gridTemplateColumns = `repeat(16, 2fr)`;
+        grid.style.gridTemplateRows = `repeat(16, 2fr)`;
+      }
+      colorGrid();
+    };
+  
+    const colorGrid = () => {
+      const allColors = document.querySelectorAll('.colorGrid');
+      for (const color of allColors) {
+        color.addEventListener('mousedown', () => {
+          isMouseDown = true;
+        });
+  
+        color.addEventListener('mouseup', () => {
+          isMouseDown = false;
+        });
+  
+        color.addEventListener('mousemove', (e) => {
+          if (isMouseDown) {
             const colorBackground = getComputedStyle(e.target).backgroundColor;
             const regex = /\d\.\d/;
-
+  
             if (colorBackground.match(regex) !== null) {
-                const colorOpacity = Number(colorBackground.match(regex));
-                e.target.style.backgroundColor = colorBackground.replace(regex, colorOpacity + 0.2);
+              const colorOpacity = Number(colorBackground.match(regex));
+              e.target.style.backgroundColor = colorBackground.replace(regex, colorOpacity + 0.2);
             }
+          }
         });
-    }
-}
-
-window.addEventListener('load', () => defaultGrid());
-
-function createGrid() {
-    if (!input.value) {
+      }
+    };
+  
+    const createGrid = () => {
+      if (!input.value) {
         alert('please set a grid size');
-    } else {
+      } else {
         grid.innerHTML = '';
-        grid.style.setProperty('grid-template-columns', `repeat(${input.value}, 2fr)`);
-        grid.style.setProperty('grid-template-rows', `repeat(${input.value}, 2fr)`);
-        place.innerHTML = input.value + 'x' + input.value;
-
+        grid.style.gridTemplateColumns = `repeat(${input.value}, 2fr)`;
+        grid.style.gridTemplateRows = `repeat(${input.value}, 2fr)`;
+        place.innerHTML = `${input.value}x${input.value}`;
+  
         for (let i = 0; i < input.value * input.value; i++) {
-            const div = document.createElement('div');
-            div.classList.add('colorGrid');
-            grid.appendChild(div);
+          const div = document.createElement('div');
+          div.classList.add('colorGrid');
+          grid.appendChild(div);
         }
         colorGrid();
-    }
-}
-
-sizeGrid.addEventListener('click', createGrid);
-
-reset.addEventListener('click', () => {
-    grid.innerHTML = '';
-    input.value = '';
-    place.innerHTML = '16x16';
-    defaultGrid();
-});
+      }
+    };
+  
+    const resetGrid = () => {
+      grid.innerHTML = '';
+      input.value = '';
+      place.innerHTML = '16x16';
+      defaultGrid();
+    };
+  
+    const init = () => {
+      reset.addEventListener('click', resetGrid);
+      sizeGrid.addEventListener('click', createGrid);
+      window.addEventListener('load', defaultGrid);
+    };
+  
+    return {
+      init
+    };
+  })();
+  
+  app.init();
+  
