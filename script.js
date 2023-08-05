217e402602d73b4c5734d211961b2ef468a93df8
Rian <riansantosjr11@gmail.com>
2023-08-04 22:23:21 -0300

    add input validator

--- script.js
+++ script.js
@@ -43,22 +43,26 @@ const app = (() => {
     };
   
     const createGrid = () => {
-      if (!input.value) {
-        alert('please set a grid size');
+      if (!input.value || input.value <= 0 || input.value.toString().charAt(0) === "0") {
+        alert('Please set a grid size');
+        input.value = '';
+      } else if (input.value >= 31) {
+        alert('Exceeded the grid limit');
+        input.value = '';
       } else {
-        grid.innerHTML = '';
-        grid.style.gridTemplateColumns = `repeat(${input.value}, 2fr)`;
-        grid.style.gridTemplateRows = `repeat(${input.value}, 2fr)`;
-        place.innerHTML = `${input.value}x${input.value}`;
+         grid.innerHTML = '';
+         grid.style.gridTemplateColumns = `repeat(${input.value}, 2fr)`;
+         grid.style.gridTemplateRows = `repeat(${input.value}, 2fr)`;
+         place.innerHTML = `${input.value}x${input.value}`;
   
-        for (let i = 0; i < input.value * input.value; i++) {
-          const div = document.createElement('div');
-          div.classList.add('colorGrid');
-          grid.appendChild(div);
-        }
-        colorGrid();
-      }
-    };
+         for (let i = 0; i < input.value * input.value; i++) {
+           const div = document.createElement('div');
+           div.classList.add('colorGrid');
+           grid.appendChild(div);
+         }
+         colorGrid();
+       }
+  };
   
     const resetGrid = () => {
       grid.innerHTML = '';
@@ -79,4 +83,3 @@ const app = (() => {
   })();
   
   app.init();
-  
